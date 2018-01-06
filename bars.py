0488d5d59dcb867b68009929b47fa3851b7a06e4
Dina <dina.draguzya@gmail.com>
2018-01-06 21:53:30 +0200

    Fixed code style issues

--- bars.py
+++ bars.py
@@ -10,13 +10,17 @@ def load_data(file_path):
 
 
 def get_biggest_bar(json_data):
-    return max(json_data,
-               key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
+    return max(
+        json_data,
+        key=lambda bar: bar['properties']['Attributes']['SeatsCount']
+    )
 
 
 def get_smallest_bar(json_data):
-    return min(json_data,
-               key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
+    return min(
+        json_data,
+        key=lambda bar: bar['properties']['Attributes']['SeatsCount']
+    )
 
 
 def calculate_distance(latitude_1, latitude_2, longitude_1, longitude_2):
@@ -25,11 +29,15 @@ def calculate_distance(latitude_1, latitude_2, longitude_1, longitude_2):
 
 
 def get_closest_bar(json_data, latitude, longitude):
-    return min(json_data,
-               key=lambda bar: calculate_distance(bar['geometry']['coordinates'][0],
-                                                  latitude,
-                                                  bar['geometry']['coordinates'][1],
-                                                  longitude))
+    return min(
+        json_data,
+        key=lambda bar: calculate_distance(
+            bar['geometry']['coordinates'][0],
+            latitude,
+            bar['geometry']['coordinates'][1],
+            longitude
+        )
+    )
 
 
 def get_bar_name(bar):
